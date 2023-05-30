803c9196306c0d57c60cfe7f9bd82d9375cee7cb
shaik amir <shaikamir2003@gmail.com>
2023-05-30 18:44:01 +0530

    commit6

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 #import cv2
 import streamlit as st
-from ultralytics import YOLO
-import openai
-from collections import defaultdict
-import pyttsx3
+#from ultralytics import YOLO
+#import openai
+#from collections import defaultdict
+#import pyttsx3
 
 engine = pyttsx3.init()
 openai.api_key = ''
